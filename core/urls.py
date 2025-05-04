cd71c01e20b3d752f72e3e89a972c1bfbed59e05
nicocastronc <nicocastronini@gmail.com>
2025-05-03 22:00:55 -0300

    borrado views from core

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from .views import TestView
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', TestView.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
